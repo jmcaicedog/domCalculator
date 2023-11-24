9478f58a7586b1023d9f43e84595f678df9e8324
Juan Maenuel Caicedo <juanmanuel@microingenieria.net>
2023-11-23 22:00:06 -0500

    Buttons reading and value showing in Output

--- calculator.js
+++ calculator.js
@@ -1,1 +1,8 @@
-console.log("hola, mundo!");
+const output = document.getElementById("output");
+const buttons = document.getElementsByClassName("calculator__key");
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", () => buttonClick(buttons[i].innerHTML));
+}
+function buttonClick(i) {
+  output.innerText = output.innerText + i;
+}
