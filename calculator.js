7d1f9962857a1b4f92b54a54823e1818e9bf9955
Juan Maenuel Caicedo <juanmanuel@microingenieria.net>
2023-11-25 11:45:04 -0500

    Typing number A and show it on output

--- calculator.js
+++ calculator.js
@@ -1,8 +1,15 @@
 const output = document.getElementById("output");
 const buttons = document.getElementsByClassName("calculator__key");
+const a = [];
+const b = [];
+const operators = ["+", "-", "x", "/", "=", "AC"];
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", () => buttonClick(buttons[i].innerHTML));
 }
+
 function buttonClick(i) {
-  output.innerText = output.innerText + i;
+  if (!operators.includes(i)) {
+    a.push(i);
+  }
+  output.innerText = a.join("");
 }
