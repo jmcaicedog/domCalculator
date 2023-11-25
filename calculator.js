993697a6bc9f84da7da3d4a22d37b1cbe0ed7cdb
Juan Maenuel Caicedo <juanmanuel@microingenieria.net>
2023-11-25 11:58:35 -0500

    Two numbers read and operator idenfified

--- calculator.js
+++ calculator.js
@@ -2,14 +2,22 @@ const output = document.getElementById("output");
 const buttons = document.getElementsByClassName("calculator__key");
 const a = [];
 const b = [];
+let operator = "+";
 const operators = ["+", "-", "x", "/", "=", "AC"];
+let operatorPressed = false;
 for (let i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", () => buttonClick(buttons[i].innerHTML));
 }
 
 function buttonClick(i) {
-  if (!operators.includes(i)) {
+  if (!operators.includes(i) && operatorPressed == false) {
     a.push(i);
+    output.innerText = a.join("");
+  } else if (!operators.includes(i) && operatorPressed == true) {
+    b.push(i);
+    output.innerText = b.join("");
+  } else {
+    operator = i;
+    operatorPressed = true;
   }
-  output.innerText = a.join("");
 }
